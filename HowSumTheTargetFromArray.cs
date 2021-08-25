051a517808f5685d9d1e9bdff20c521e65856d66
Arkadiusz Waw <arekwaw21@gmail.com>
2021-08-25 23:54:21 +0200

    Added How To Sum To The Target

---
+++ HowSumTheTargetFromArray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Programming
+{
+    class HowSumTheTargetFromArray
+    {
+        public int HowSum(int target, int n, int[] numbers)
+        {
+            //Get from techiedlight.com
+
+            // base case: if a target is found
+            if (target == 0)
+            {
+                return 1;
+            }
+
+            // base case: no elements are left
+            if (n < 0)
+            {
+                return 0;
+            }
+
+            // 1. ignore the current element
+            int exclude = HowSum(target, n - 1, numbers);
+
+            // 2. Consider the current element
+
+            // 2.1. Subtract the current element from the target
+            // 2.2. Add the current element to the target
+            int include = HowSum(target - numbers[n], n - 1, numbers) +
+                        HowSum(target + numbers[n], n - 1, numbers);
+
+            // Return total count
+            return exclude + include;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace Dynamic_Programming
             watch.Stop();
             Console.WriteLine(($"Execution Time: {watch.ElapsedMilliseconds} ms"));
 
-            Console.ReadKey();
+            Console.ReadKey(); Console.WriteLine("NUMBER OF WAYS IN A GRID");
             //PROGRAM 2 -> GridTravel -> Slow version (fast not working) 
             GridTravel gridTravel = new GridTravel();
             Console.WriteLine(gridTravel.Grid(1, 1));      //1
@@ -34,7 +34,7 @@ namespace Dynamic_Programming
 
             //TODO: NOT WORKING Console.WriteLine(gridTravel.Grid(18, 18));    //2333606220
 
-            Console.ReadKey();
+            Console.ReadKey(); Console.WriteLine("CAN SUM TO THE TARGET");
 
             CanSumTheNumberFromArray cs = new CanSumTheNumberFromArray();
             int[] arr1 = { 2, 3 }; Console.WriteLine(cs.Sum(7, arr1)); //false
@@ -43,18 +43,28 @@ namespace Dynamic_Programming
             Console.WriteLine();
             int[] arr4 = { 2, 3,5 }; Console.WriteLine(cs.Sum(8, arr4)); //true
             int[] arr4_4 = { 1,2,5 }; Console.WriteLine(cs.Sum(10, arr4_4)); //false 
+            int[] arr4_8 = { 1,2,3,4,5 }; Console.WriteLine(cs.Sum(12, arr4_8)); //true 
             Console.WriteLine();
             int[] arr5 = { 7, 14 }; Console.WriteLine(cs.Sum(300, arr5)); //false
             int[] arr5_5_5 = { 7, 14 }; Console.WriteLine(cs.Sum(25600, arr5_5_5)); //false
             Console.WriteLine();
-            int[] arr5_5 = { 1,2,3,5,5,6,7,8,9,9999,30595,99573}; Console.WriteLine(cs.Sum(300, arr5_5)); //true
+            int[] arr5_5 = { 1, 2, 3, 5, 5, 6, 7, 8, 9, 9999, 30595, 99573 }; Console.WriteLine(cs.Sum(300, arr5_5)); //SHOULD BE FALSE, idk why true
+
+            int[] arr5_5_10 = { -6, -51,53,4 }; Console.WriteLine(cs.Sum(0, arr5_5_10)); //true
             int[] arr6 = { 7, 14,300 }; Console.WriteLine(cs.Sum(300, arr6)); //true
             int[] arr7 = { 1, 14, 286 }; Console.WriteLine(cs.Sum(300, arr7)); //true
 
-            Console.ReadKey();
+            Console.ReadKey(); Console.WriteLine("HOW TO SUM TO THE TARGET");
+
+            HowSumTheTargetFromArray hs = new HowSumTheTargetFromArray();
+            int[] array1 = { 1, 14, 286 }; Console.WriteLine(hs.HowSum(28, array1.Length-1,array1)); //2 ways  NOT WORKING, idk why
+            int[] array2 = { 5, 3, -6, 2 }; Console.WriteLine(hs.HowSum(6, array2.Length-1,array2)); //4 ways
+            int[] array3 = { 44, 12,1000 }; Console.WriteLine(hs.HowSum(15, array3.Length-1,array3)); //0
 
 
         }
+
+
 
         public static int FibRecursive(int number)
         {
